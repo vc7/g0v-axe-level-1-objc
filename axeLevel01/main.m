435c06080b44a033f81cb96cc07d3410aa474018
Vincent Chen <vince78718@gmail.com>
2014-04-22 22:29:35 +0800

    Initial Commit

---
+++ axeLevel01/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  axeLevel01
+//
+//  Created by vincent on 2014/04/22.
+//  Copyright (c) 2014年 vc7. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
